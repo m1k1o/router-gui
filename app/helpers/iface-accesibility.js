ea902738c0de6b7969d661547f597f91b0794083
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-11 02:09:06 +0100

    Improved interface accesibility helpers.

--- app/helpers/iface-accesibility.js
+++ app/helpers/iface-accesibility.js
@@ -1,20 +1,48 @@
 Vue.component('interface-input', {
-    props: ['value'],
+    props: {
+        value: {},
+        running_only: {
+            type: Boolean,
+            default: null
+        }
+    },
+    computed: {
+        interfaces() {
+            return this.$store.state.interfaces.table;
+        }
+    },
     template: `
-        <input
+        <select
             class="form-control"
             v-bind:value="value"
             v-on:input="$emit('input', $event.target.value)"
         >
+            <option value="">--not selected--</option>
+            <optgroup v-if="running_only !== false" label="Running">
+                <option v-for="(interface, id) in interfaces" v-if="interface.running" :value="id">{{ interface.friendly_name }}</option>
+            </optgroup>
+            <optgroup v-if="running_only !== true" label="Not Running">
+                <option v-for="(interface, id) in interfaces" v-if="!interface.running" :value="id">{{ interface.friendly_name }}</option>
+            </optgroup>
+        </select>
     `
 })
 
 Vue.component('interface-show', {
     props: ['id'],
+    computed: {
+        interface() {
+            return this.$store.state.interfaces.table[this.id];
+        }
+    },
     template: `
-        <span class="eth">
+        <span v-if="id" class="eth" :title="interface.friendly_name + '\\n' + interface.mac + '\\n' + interface.ip">
+            <img v-bind:src="interface.running ? 'images/eth_active.png' : 'images/eth.png'">
+            <span class="id"> {{ id }} </span>
+        </span>
+        <span v-else class="eth">
             <img src="images/eth.png">
-            <span class="id"> {{ id || '-' }} </span>
+            <span class="id">?</span>
         </span>
     `
 })
