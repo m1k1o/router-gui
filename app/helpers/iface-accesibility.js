f7d8ffd4db4d6e367a69fe6ce9128502bf9293cb
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-02-20 20:03:30 +0100

    new ui.

--- app/helpers/iface-accesibility.js
+++ app/helpers/iface-accesibility.js
@@ -4,11 +4,22 @@ Vue.component('interface-input', {
         running_only: {
             type: Boolean,
             default: null
+        },
+        readonly: {
+            type: Boolean,
+            default: false
+        },
+        placeholder: {
+            type: String,
+            default: ""
         }
     },
     computed: {
         interfaces() {
             return this.$store.state.interfaces.table;
+        },
+        interface() {
+            return this.model ? this.$store.state.interfaces.table[this.model] : '';
         },
         model: {
             get() {
@@ -19,7 +30,25 @@ Vue.component('interface-input', {
             }
         }
     },
+    data: () => ({
+        dropdown_opened: false
+    }),
     template: `
+        <div class="input-group">
+            <span class="form-control" @click="dropdown_opened = !dropdown_opened">{{ interface.friendly_name }}</span>
+            <div class="dropdown-menu" v-bind:class="{'show': dropdown_opened && !readonly}" style="width:100%;">
+                <a class="dropdown-item" href="#" @click="Select('')"><i>Not Selected</i></a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" v-for="(interface, id) in interfaces" v-if="interface.ip" @click="Select(id)">
+                    <interface-show :id="id" class="float-left mr-3"></interface-show>
+                    <span v-bind:title="interface.description">{{ interface.friendly_name }}</span><br><small v-bind:title="interface.name">{{ interface.mac }}</small>
+                </a>
+            </div>
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" @click="dropdown_opened = !dropdown_opened">{{ dropdown_opened ? '&#9650;' : '&#9660;' }}</button>
+            </div>
+        </div>
+        <!--
         <select class="form-control" v-model="model">
             <option value="">--not selected--</option>
             <optgroup v-if="running_only !== false" label="Running">
@@ -29,7 +58,14 @@ Vue.component('interface-input', {
                 <option v-for="(interface, id) in interfaces" v-if="!interface.running" :value="id">{{ interface.friendly_name }}</option>
             </optgroup>
         </select>
-    `
+        -->
+    `,
+    methods: {
+        Select(id) {
+            this.model = id;
+            this.dropdown_opened = false;
+        }
+    }
 })
 
 Vue.component('interface-show', {
--- app/helpers/ip-mask-inputs.js
+++ app/helpers/ip-mask-inputs.js
@@ -1,22 +1,94 @@
 Vue.component('ip-address-input', {
-    props: ['value'],
+    props: {
+        value: {},
+        readonly: {
+            type: Boolean,
+            default: false
+        },
+        placeholder: {
+            type: String,
+            default: ""
+        }
+    },
+    computed: {
+        interfaces() {
+            return this.$store.state.interfaces.table;
+        },
+        arps() {
+            return this.$store.state.arp.table;
+        },
+        model: {
+            get() {
+                return this.value == null ? '' : this.value
+            },
+            set(value) {
+                if(value == "") {
+                    this.select = false;
+                }
+                this.$emit('input', value)
+            }
+        }
+    },
+    data: () => ({
+        dropdown_opened: false,
+        latest: []
+    }),
     template: `
-        <input
-            class="form-control"
-            v-bind:value="value"
-            v-on:input="$emit('input', $event.target.value)"
-        >
-    `
+        <div class="input-group">
+            <input
+                class="form-control"
+
+                v-model="model"
+                :placeholder="placeholder"
+                :readonly="readonly"
+            />
+            <div class="dropdown-menu" v-bind:class="{'show': dropdown_opened}" style="width:100%;">
+                <a class="dropdown-item" href="#" v-for="interface in interfaces" v-if="interface.ip" @click="Put(interface.ip)">{{ interface.ip }}</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" v-for="arp in arps" v-if="arp.ip" @click="Put(arp.ip)">{{ arp.ip }}</a>
+            </div>
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" @click="dropdown_opened = !dropdown_opened">{{ dropdown_opened ? '&#9650;' : '&#9660;' }}</button>
+            </div>
+        </div>
+    `,
+    methods: {
+        Put(ip) {
+            this.model = ip;
+            this.dropdown_opened = false;
+        }
+    }
 })
 
 Vue.component('ip-mask-input', {
-    props: ['value'],
+    props: {
+        value: {},
+        readonly: {
+            type: Boolean,
+            default: false
+        },
+        placeholder: {
+            type: String,
+            default: "255.255.255.255"
+        }
+    },
+    computed: {
+        model: {
+            get() {
+                return this.value == null ? '' : this.value
+            },
+            set(value) {
+                this.$emit('input', value)
+            }
+        }
+    },
     template: `
         <input
             class="form-control"
-            v-bind:value="value"
-            v-on:input="$emit('input', $event.target.value)"
-            placeholder="255.255.255.255"
-        >
+
+            v-model="model"
+            :placeholder="placeholder"
+            :readonly="readonly"
+        />
     `
 })
