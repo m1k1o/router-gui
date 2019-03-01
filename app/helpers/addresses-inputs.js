44953fc7a87321a50bd426e32b5f2b5031634dfd
Miroslav Šedivý <sedivy.miro@gmail.com>
2019-03-02 00:14:02 +0100

    Validation as Function + port input.

--- app/helpers/addresses-inputs.js
+++ app/helpers/addresses-inputs.js
@@ -16,7 +16,7 @@ var Input_Validation_Mixin = {
         value: {
             immediate: true,
             handler() {
-                this.is_valid = (!this.required && (this.value == "" || this.value == null) ? true : this.regex.test(this.value))
+                this.is_valid = (!this.required && (this.value == "" || this.value == null) ? true : this.test(this.value))
             }
         },
         is_valid: {
@@ -33,7 +33,7 @@ Vue.component('ip-address-input', {
 
     mixins: [Input_Validation_Mixin],
     data: () => ({
-        regex: /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/
+        test: (value) => /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/.test(value)
     }),
     
     template: `
@@ -51,7 +51,7 @@ Vue.component('ip-mask-input', {
 
     mixins: [Input_Validation_Mixin],
     data: () => ({
-        regex: /^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$/
+        test: (value) => /^(((255\.){3}(255|254|252|248|240|224|192|128|0+))|((255\.){2}(255|254|252|248|240|224|192|128|0+)\.0)|((255\.)(255|254|252|248|240|224|192|128|0+)(\.0+){2})|((255|254|252|248|240|224|192|128|0+)(\.0+){3}))$/.test(value)
     }),
     
     template: `
@@ -70,7 +70,25 @@ Vue.component('mac-input', {
 
     mixins: [Input_Validation_Mixin],
     data: () => ({
-        regex: /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/
+        test: (value) => /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/.test(value)
+    }),
+
+    template: `
+        <input
+            class="form-control"
+            v-bind:value="value"
+            v-on:input="$emit('input', $event.target.value)"
+            :style="!is_valid ? 'border-color: #dc3545' : ''"
+        >
+    `
+})
+
+Vue.component('port-input', {
+    props: ['value'],
+
+    mixins: [Input_Validation_Mixin],
+    data: () => ({
+        test: (value) => !isNaN(value) && value > 0 && value < 65536
     }),
 
     template: `
